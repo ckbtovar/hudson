481085214cc3ce04827f6f466cf5234dd3a089dc
Brian Tovar <brian.tovar@creditkarma.com>
2014-04-23 17:23:13 -0700

    Adding an Example class to perform code coverage

---
+++ src/Example.php
@@ -0,0 +1,6 @@
+<?
+class Example {
+    public function add($a, $b) {
+        return $a + $b;
+    }
+}
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,7 +1,9 @@
 <?
+require('../src/Example.php');
 class ExampleTest extends PHPUnit_Framework_TestCase {
 
   public function testFoo() {
-    $this->assertEquals(2, 1 + 1);
+    $example = new Example();
+    $this->assertEquals(2, $example->add(1,1));
   }
 }
